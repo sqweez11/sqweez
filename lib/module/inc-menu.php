39aa826badfb4c2b4bdcd95dac9557b876c203e1
sqweez11 <132778497+sqweez11@users.noreply.github.com>
2023-05-06 18:16:32 +0000

    A

---
+++ lib/module/inc-menu.php
@@ -0,0 +1,349 @@
+<div cl
+ass='menu-con-release'>
+	<span>Website Release Preview 2023.05.05</span>
+</div>
+<div class='mini-menu-con-dimmer'>
+	<div class='mini-menu-con-container'>
+		<div class='mini-menu-ico-.landing-ico-logo'>
+		</div>
+		<div class='mini-menu-tx1-block'> 
+			<div class='mini-menu-emp-block'>
+			</div>
+			<h2>Ответственность</h2>
+		</div>
+		<div class='mini-menu-tx2-block'>
+			<p>
+				 Мы не несем никакой <b>ответственности</b>  за ваши <u>действия</u>. Если вы нарушаете <u>политические</u> правила игры это вся <b>ответственность</b> приходиться только на вас.<br>
+				<br>
+				  Мы даем вам <b>возможности</b> но не несем никакой <b>ответственности</b> за любые ваши действия которые могут привести к <u>нарушению политической концидиденциальности компаний</u>. Делайте все тщательно обдумав и помните о последствиях. Перед использывание нашего ПО обязательно ознакомьтесь с нашей <a href="/policy" target="_blank">политической концидиденциальностю</a>.
+			</p>
+		</div>
+		<div class="mini-menu-btn-agree">
+			<h2>Я понимаю</h2>
+		</div>
+	</div>
+</div>
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+		<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>This feature is currently not yet available</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div> 
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Dark Theme - On/Off</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-transparency">
+		<div class="settings-menu-ico-transparency">
+		</div>
+		<div class="settings-menu-btn-button btn-transparency">
+			<span>Transparency - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-particles">
+		<div class="settings-menu-ico-particles">
+		</div>
+		<div class="settings-menu-btn-button btn-particles">
+			<span>Particles - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-pulsate">
+		<div class="settings-menu-ico-pulsate">
+		</div>
+		<div class="settings-menu-btn-button btn-pulsate">
+			<span>Pulsate - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-waves">
+		<div class="settings-menu-ico-waves">
+		</div>
+		<div class="settings-menu-btn-button btn-waves">
+			<span>Waves - On</span>
+		</div>
+	</div> 
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-theme settings-menu-lower" style="border-top: solid 1px rgba(0,0,0,.02);">
+		<div class="settings-menu-ico-theme">
+		</div>
+		<div class="settings-menu-btn-button btn-theme">
+			<span>Change Theme</span>
+		</div>
+	</div>
+</div>
+<div class="menu-btn-backtotop"> 
+</div> 
+<div class='menu-con-container'> 
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+	</div>
+	<div class="menu-con-outer"> 		
+	<div class="menu-con-inner">
+			<div class="menu-con-logo"> 
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div> 
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+		<!-- <a href='/blog'>
+			<div class="menu-btn-select">
+				<span>Blog</span>
+			</div>
+			</a>-->
+			<a href='/about'>
+			<div class="menu-btn-select">
+				<span>О нас</span>
+			</div>
+			</a>
+			<!--<a href='/compatibility'>
+			<div class="menu-btn-select">
+				<span>Compatibility</span>
+			</div>
+			</a>-->
+			<a href='/software'>
+			<div class="menu-btn-select">
+				<span>Наш товар</span>
+			</div>
+			</a> 
+			
+			<!--<a href='/software'>
+			<div class="menu-btn-select">
+				<span>Наш Товар</span>
+			</div>
+			</a> -->
+			
+			<a href='/policy'>
+			<div class="menu-btn-select">
+				<span>Политика</span>
+			</div>
+			</a>
+			
+			<a href='/faq'>
+			<div class="menu-btn-select">
+				<span>Вопросы</span>
+			</div>
+			</a>
+			<div class="menu-con-divider">
+			</div>
+		<!-- 	<a href='https://github.com/rpcs3' target="_blank">
+			<div class="menu-btn-select">
+				<span>GitHub</span>
+			</div>
+			</a>
+		<!--	<a href='https://forums.rpcs3.net' target="_blank">
+			<div class="menu-btn-select">
+				<span>Forum</span>
+			</div>
+			</a> -->
+			<a href='https://wiki.rpcs3.net'>
+			<div class="menu-btn-select">
+				<span>Wiki</span>
+			</div>
+			</a>
+			<a href='https://discord.gg/Sg2NFcptDW' target="_blank">
+			<div class="menu-btn-select">
+				<span>Discord</span>
+			</div>
+			</a>
+			<a href='https://www.youtube.com/channel/UC6qglpruvb7JoTRq64G1dqA' target="_blank">
+			<div class="menu-btn-select">
+				<span>YouTube</span>
+			</div>
+			</a>
+			<div class="support-con-menu">
+				<a href="https://www.patreon.com/Nekotekina" target="_blank">
+				<div class="support-ico-menu">
+				</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Main</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div> 
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Home</span>
+				</div>
+				</a> 
+				<a href="/blog">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Blog</span>
+				</div>
+				</a>
+				<a href="/About">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>About</span>
+				</div>
+				</a>
+				<a href="/compatibility">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Compatibility</span>
+				</div>
+				</a>
+				<a href="/download">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Download</span>
+				</div>
+				</a>
+				<a href="/policy">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Политика</span>
+				</div>
+				</a>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Наш Товар</span>
+				</div>
+				</a>
+				<a href="/faq">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>FAQs</span>
+				</div>
+				</a>
+				<a href="https://forums.rpcs3.net" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Forum</span>
+				</div>
+				</a>
+				<a href='https://wiki.rpcs3.net'>
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Wiki</span>
+				</div>
+				</a>
+				<div class='mobile-menu-tx1-group'>
+					<span>Internal</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/branding">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Branding</span>
+				</div>
+				</a>
+				<a href="/disclaimer">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Disclaimer</span>
+				</div>
+				</a>
+				<div class='mobile-menu-tx1-group'>
+					<span>External</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="https://github.com/rpcs3" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>GitHub</span>
+				</div>
+				</a>
+				<a href="https://discord.gg/Sg2NFcptDW" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Discord</span>
+				</div>
+				</a>
+				<a href="https://www.youtube.com/channel/UC6qglpruvb7JoTRq64G1dqA" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>YouTube</span>
+				</div>
+				</a>
+				<div class='mobile-menu-tx1-group'>
+					<span>Support</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="https://www.patreon.com/Nekotekina">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Patreon</span>
+				</div>
+				</a>
+				<a href="/alipay">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Alipay</span>
+				</div>
+				</a>
+				<div class='mobile-menu-btn-spacer'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
